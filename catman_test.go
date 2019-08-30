0fa836055d23fbaf862b8093a510572e37db0cea
Shane Xu <xusheng0711@gmail.com>
2019-08-30 15:54:13 +0800

    add test case

--- catman_test.go
+++ catman_test.go
@@ -1,15 +1,62 @@
 package catman
 
 import (
+	"reflect"
 	"testing"
 )
 
-func test_findCandidateJ(t *testing.T) {
-	c, err := findCandidateJ([]candidate{{"_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5}}, candidate{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5})
-	if err != nil {
-		t.Fatal(c)
+func Test_findCandidateJ(t *testing.T) {
+	type args struct {
+		cs   []candidate
+		self candidate
 	}
-	if c != nil {
-		t.Fatal("c is not nil")
+	tests := []struct {
+		name    string
+		args    args
+		want    *candidate
+		wantErr bool
+	}{
+		{
+			"case no j",
+			args{
+				[]candidate{{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5}},
+				candidate{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5},
+			},
+			nil,
+			false,
+		},
+		{
+			"case not in",
+			args{
+				[]candidate{{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5}},
+				candidate{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000004", 4},
+			},
+			nil,
+			true,
+		},
+		{
+			"case normal",
+			args{
+				[]candidate{
+					{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5},
+					{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000004", 4},
+				},
+				candidate{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000005", 5},
+			},
+			&candidate{"/testle/_c_5ef0d137074f090569e4f22732f6fd0f-0000000004", 4},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := findCandidateJ(tt.args.cs, tt.args.self)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("findCandidateJ() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findCandidateJ() got = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
